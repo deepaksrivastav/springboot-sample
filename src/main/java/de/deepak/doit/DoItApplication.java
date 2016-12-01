9128b9c89234750460b4667984a909a354f04e12
Deepak Srinathan <dsrinathan@smartshifttech.com>
2016-12-01 15:37:53 +0100

    Return ip address on main page

--- src/main/java/de/deepak/doit/DoItApplication.java
+++ src/main/java/de/deepak/doit/DoItApplication.java
@@ -1,22 +1,25 @@
 package de.deepak.doit;
 
-import com.google.common.base.Predicates;
-import de.deepak.doit.controller.NotesController;
-import de.deepak.doit.controller.ToDoItemController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.base.Predicates;
+
+import de.deepak.doit.controller.NotesController;
+import de.deepak.doit.controller.ToDoItemController;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -25,7 +28,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DoItApplication {
     @RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+    	InetAddress ipAddress;
+		try {
+			ipAddress = InetAddress.getLocalHost();
+			return ipAddress.getHostAddress();
+		} catch (UnknownHostException e) {
+			return "Hello Docker World";
+		}
 	}
 
     public static void main(String[] args) {
